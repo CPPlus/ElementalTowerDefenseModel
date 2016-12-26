30ea84b1b2141813e85cb7e9164af7bf43c5e036
Людмил Григоров <grigorovludmil@gmail.com>
2016-12-26 23:39:49 +0200

    Quick fixes before clone test.

--- ElementalTowerDefenseModel/Concrete/CrawlingHorror.cs
+++ ElementalTowerDefenseModel/Concrete/CrawlingHorror.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ElementalTowerDefenseModel.Concrete
+namespace ElementalTowerDefenseModel
 {
     public class CrawlingHorror : Monster
     {
