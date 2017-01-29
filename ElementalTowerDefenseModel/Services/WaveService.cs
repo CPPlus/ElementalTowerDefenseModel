9c44c6cf00a1e17051ddaf85f597203381e3d32b
Людмил Григоров <grigorovludmil@gmail.com>
2017-01-29 16:58:20 +0200

    Updated WaveService

--- ElementalTowerDefenseModel/Services/WaveService.cs
+++ ElementalTowerDefenseModel/Services/WaveService.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ElementalTowerDefenseModel
 {
-    class WaveService
+    public class WaveService
     {
+        public int WavesSpawned { get; private set; }
+        public Wave CurrentWave { get; private set; }
+
+        public WaveService()
+        {
+            WavesSpawned = 0;
+        }
+
+        public Wave Spawn(MonsterType type, int count)
+        {
+            Wave wave = new Wave();
+            for (int i = 0; i < count; i++)
+            {
+                Monster monster = Instantiate(type);
+                if (monster != null)
+                    wave.Add(monster);
+            }
+            CurrentWave = wave;
+            WavesSpawned++;
+            return wave;
+        }
+
+        public Wave Spawn(WaveData data)
+        {
+            Wave wave = new Wave();
+            MonsterType[] monsterTypes = data.MonsterTypes;
+            foreach (MonsterType type in monsterTypes)
+            {
+                Monster monster = Instantiate(type);
+                if (monster != null)
+                    wave.Add(monster);
+            }
+            CurrentWave = wave;
+            WavesSpawned++;
+            return wave;
+        }
+
+        private Monster Instantiate(MonsterType type)
+        {
+            Monster monster = null;
+            switch (type)
+            {
+                case MonsterType.CRAWLING_HORROR: monster = new CrawlingHorror(); break;
+            }
+            return monster;
+        }
     }
 }
