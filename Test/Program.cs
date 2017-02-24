b6a3bd162ddb6d491b8cf9d92521d36ab42bcfa6
Людмил Григоров <grigorovludmil@gmail.com>
2017-02-24 20:12:22 +0200

    Don't know why all files are marked as changed, but I will commit so that it looks better.

---
+++ Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using ElementalTowerDefenseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        private static int a;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void PrintVitalInfo(LivingEntity entity)
+        {
+            Console.WriteLine("Health: " + entity.HealthComp.Health.Points);
+            Console.WriteLine("Is Dead: " + entity.HealthComp.IsDead);
+        }
+    }
+}
